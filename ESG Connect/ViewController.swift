41e02bedd31663139e25f306e62b7d142a5922f7
Can Duru <canduru2004@gmail.com>
2023-07-07 11:08:29 +0300

    Initial Commit

---
+++ ESG Connect/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ESG Connect
+//
+//  Created by Can Duru on 7.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
